b6db701d93ba62aa4f694f3972f3df52a59a4d9e
Paige <paigechoi0701@gmail.com>
2020-09-01 11:02:56 +0900

    create schema for graphql

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ingredients'
+    'ingredients',
+    'graphene_django'
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'ingredients.graphql.schema.schema'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ ingredients/graphql/schema.py
@@ -0,0 +1,34 @@
+from graphene_django import DjangoObjectType
+import graphene
+from ingredients.models import Category, Ingredient
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'ingredients')
+
+class IngredientType(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'notes', 'category')
+
+
+class Query(graphene.ObjectType):
+    all_ingredients = graphene.List(IngredientType)
+    all_categories = graphene.List(CategoryType)
+    category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
+
+    def resolve_all_ingredients(self, info):
+        return Ingredient.objects.select_related("category").all()
+
+    def resolve_all_categories(self, info):
+        return Category.objects.all()
+
+    def resolve_category_by_name(self, info, name):
+        try:
+            return Category.objects.get(name=name)
+        except Category.DoesNotExist:
+            return None
+
+schema = graphene.Schema(query=Query)
